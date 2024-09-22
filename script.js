5d2f8e27d2468c103c948c64380a75ff07c26d4e
Hareesh123NH <162862005+Hareesh123NH@users.noreply.github.com>
2024-09-22 14:37:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+
+bands.sort((a,b) => {
+	 a=a.replace(/^(a |an |the )/i, '');
+	b=b.replace(/^(a |an |the )/i, '');
+	return a.localeCompare(b);
+});
+
+const ul=document.getElementById("band");
+bands.forEach((band)=>{
+	const li=document.createElement("li");
+	li.innerText=band;
+	ul.appendChild(li);
+})
